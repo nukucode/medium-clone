d2505c2df9af06454f8f64d9c4b968929d5ea880
Ankit Yadav <therogersak@gmail.com>
2024-09-23 04:19:27 +0530

    medium-build

---
+++ components/Posts.tsx
@@ -0,0 +1,36 @@
+import { urlFor } from "@/sanity"
+import { Post } from "@/typing"
+import Image from "next/image"
+import Link from "next/link"
+import React from "react"
+
+interface Posts {
+    posts: [Post]
+}
+
+export function Posts({ posts }: Posts) {
+    console.log('🤗', posts)
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+            {posts && React.Children.toArray(posts.map((post) => <Link href={`/post/${post.slug.current}`}>
+                <div className="border rounded-lg group cursor-pointer overflow-hidden">
+                    <Image src={urlFor(post.mainImage.asset._ref).url()!} alt={post.title}
+                        width={1000}
+                        height={1000}
+                        style={{ width: '100%', height: 'auto' }}
+                    />
+                    <div className="flex justify-between bg-white p-5">
+                        <div>
+                            <p className="text-lg font-bold">{post.title}</p>
+                            <p className="text-xs">{post.description}</p>
+                        </div>
+                        <div>
+                            <Image className="w-12 h-12 rounded-full" width={100} height={100} src={urlFor(post.author.image.asset._ref).url()!} alt={post.author.name} />
+                        </div>
+                    </div>
+                </div>
+            </Link>))}
+        </div>
+    )
+}
+
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["links.papareact.com", "upload.wikimedia.org", "cdn.sanity.io"],
+  },
+};
 
 export default nextConfig;
